3024b3a02030370d25a6a643c79a6790957df378
lrodenyoder <rodenlau000@gmail.com>
2022-04-09 11:16:22 -0500

    add get user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const { User, Post, Vote, Comment } = require("../../models");
+
+//get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//get user by id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: { id: req.params.id },
+    })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "User not found" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
