9007bbb04f68e2abbea31bace2fa21969d467c8c
lrodenyoder <rodenlau000@gmail.com>
2022-04-13 21:51:53 -0500

    begin to set up hbs

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
-// const path = require("path");
-// const exphbs = require("express-handlebars");
+const path = require("path");
+//enable handlebars
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ });
+
 // const helpers = require("./utils/helpers");
-// const hbs = exphbs.create({ helpers });
 
 // const session = require("express-session");
 // const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -24,9 +26,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+//allow files to be sent as static assets
+app.use(express.static(path.join(__dirname, "public")));
+//set up to use handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 // app.use(session(sess));
 
 // turn on routes
