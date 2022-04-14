0b4a32ae688b040799674bdf33997ab816639638
lrodenyoder <rodenlau000@gmail.com>
2022-04-13 21:29:31 -0500

    update post route to include comment model

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,8 +5,16 @@ const sequelize = require("../../config/connection");
 router.get("/", (req, res) => {
     Post.findAll({
         order: [["created_at", "DESC"]],
-        attributes: ["id", "post_url", "title", "created_at"],
+        attributes: ["id", "post_url", "title", "contents", "created_at"],
         include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
             {
                 model: User,
                 attributes: ["username"],
@@ -22,26 +30,40 @@ router.get("/", (req, res) => {
 
 router.get('/:id', (req, res) => {
     Post.findOne({
-        where: { id: req.params.id },
-        attributes: ["id", "post_url", "title", "created_at"],
-        include: [
-            {
-                model: User,
-                attributes: ["username"],
-            },
-        ],
+      where: { id: req.params.id },
+      attributes: ["id", "post_url", "title", "contents", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
     })
-        .then((dbPostData) => {
-            if (!dbPostData) {
-                res.status(404).json({ message: "Post not found." });
-                return;
-            }
-            res.json(dbPostData);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json(err);
-        });
+      .then((dbPostData) => {
+        if (!dbPostData) {
+          res.status(404).json({ message: "Post not found." });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
 });
 
 router.post('/', (req, res) => {
