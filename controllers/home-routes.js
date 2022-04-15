b118523aa738dfef3ac1a2fd349adb6f6fb35352
lrodenyoder <rodenlau000@gmail.com>
2022-04-14 22:13:13 -0500

    set up post and comment views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,4 +37,39 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ["id", "post_url", "title", "contents", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "Post not found." });
+        return;
+      }
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      res.render("post", { post });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
