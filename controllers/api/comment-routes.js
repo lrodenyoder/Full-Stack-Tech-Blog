a33c3701ca3c4f4d5d71fe7ae2f7ded3be0af459
lrodenyoder <rodenlau000@gmail.com>
2022-04-14 22:24:09 -0500

    only logged in users can add comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,16 +11,19 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  if (req.session) {
     Comment.create({
         comment_text: req.body.comment_text,
         post_id: req.body.post_id,
-        user_id: req.body.user_id,
+        //take user id from session instead of body
+        user_id: req.session.user_id,
     })
         .then((dbCommentData) => res.json(dbCommentData))
         .catch((err) => {
             console.log(err);
             res.status(400).json(err);
         });
+  } else { console.log('not logged in')}
 });
 
 router.delete("/:id", (req, res) => {
